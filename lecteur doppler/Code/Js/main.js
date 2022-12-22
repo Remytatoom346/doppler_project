49a0b3612cc9b1067fd4b873d54f3649ddfe20df
Remytatoom346 <remydardennevansse@gmail.com>
2022-12-22 16:01:44 +0100

    javaS

--- lecteur doppler/Code/Js/main.js
+++ lecteur doppler/Code/Js/main.js
@@ -11,6 +11,53 @@ btn_burger.addEventListener('click', (e) => {
 btn_close.addEventListener('click', (e) => {
     div_playlist.style.display = 'none';
 });
+// LIGHT MODE DARK MODE
+
+
+let btn_dark = document.querySelector('.icon_color_set');
+let body_color = document.getElementById('body_color');
+let titre_main = document.querySelectorAll('.titre');
+let text_card = document.querySelectorAll('.text_card');
+let footer = document.querySelector('.container_footer');
+let texte_card_titre = document.querySelectorAll('.texte_card_titre')
+let a = 0; 
+
+
+
+// change la couleur du site (marche pas)
+btn_dark.addEventListener('click', (e) => { 
+    if (a==0) {{
+        body_color.style.backgroundColor = '#ffff';
+        titre_main.forEach(element => {
+            element.style.color = '#2F195F';
+        });
+        text_card.forEach(element=>{
+            element.style.color = '#2F195F';
+        })
+        texte_card_titre.forEach(element=>{
+            element.style.color = '#2F195F';
+        })
+
+        footer.style.background = 'linear-gradient(180deg, rgba(187,135,255,10) 20%, rgba(83,60,113,1) 60%)';
+        a++;
+    }}else{
+        body_color.style.backgroundColor = '#161616';
+
+        titre_main.forEach(element => {
+            element.style.color = '#966ECD';
+        });
+
+        text_card.forEach(element=>{
+            element.style.color = '#ffff';
+        })
+        texte_card_titre.forEach(element=>{
+            element.style.color = '#ddd9e3';
+        });
+        footer.style.background = 'linear-gradient(0deg, rgba(187, 135, 255, 1) 30%, rgba(47, 25, 95, 1) 70%, rgba(22, 22, 22, 1) 100%)';
+        a = 0
+    }
+}
+    )
 
 // GESTION DE L'c_audio
 
@@ -26,6 +73,7 @@ const Titre_son = document.querySelector('.titre_text_footer');
 const timer_son = document.querySelector('.time_text_footer');
 const cover_footer = document.querySelector('.cover_footer');
 const c_audio = document.querySelector('.audio');
+const cover_none = document.querySelector('.cover_none')
 
 // SELECTION DES MUSIQUES
 const list = [
@@ -66,6 +114,13 @@ const list = [
     },
 ]
 
+const list_deux = [
+    {
+        nom : "No Song",
+        image : 'cover_none'
+    }
+]
+
 // 
 
 
@@ -85,15 +140,37 @@ function demar_musique(list) {
     c_audio.play()
 }
 
+function no_song() {
+cover_none.list_deux;
+c_audio.currentTime=0;
+c_audio.pause()
+clearInterval(interval);
+Titre_son.textContent=list_deux.nom
+}
+
 btn_play.addEventListener('click',(e)=>
 {
-    play_musique()
+    play_musique();
+    interval = setInterval((e)=>{
+        musique_timer();
+    }, 1000);
+    cover_none.style.display='none'
+    cover_footer.style.display='block'
 });
 
 btn_pause.addEventListener('click',(e)=>
 {
     c_audio.pause();
+    clearInterval(interval);
 });
+
+btn_stop.addEventListener("click", (e) => {
+    no_song();
+    cover_footer.style.display='none'
+    cover_none.style.display='block';
+    clearInterval(interval);
+});
+
 
 
 let i = 1;
@@ -122,5 +199,34 @@ function next_musique(){
 
 btn_next.addEventListener('click',next_musique);
 
+// INPUT RANGE VOLUME
+let vol = document.querySelector("#vol");
+
+vol.addEventListener('click', (e) => {
+   c_audio.volume = vol.value/100; 
+});
+
+//INPUT RANGE TIME
+
+let timer = document.querySelector('.time_text_footer')
+let total_timer = document.querySelector('.total_time_text_footer')
+let interval;
+
+
+function musique_timer() {
+    let time = c_audio.currentTime;
+
+    let minutes = parseInt(time / 60, 10);
+    let seconds = parseInt(time % 60, 10);
+    if (seconds < 10) {
+        timer.innerText = minutes + ":0" + seconds;
+        time++;
+    } else {
+        timer.innerText = minutes + ":" + seconds;
+        time++;
+    }
+};
+
+timer.addEventListener('timeupdate', timer);
 
 
